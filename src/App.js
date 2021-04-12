0fdc65164f5cf0477ad8a3ff5112b1184edbd12d
Pranjal <jiv4389@gmail.com>
2021-04-12 16:33:45 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App(props){
         <Counter 
             date="2021-01-10T14:48:00" 
             timerStyle={{marginTop:'10px', width:'auto'}} 
-            counterStyle={{fontSize:'15px', color:'grey', border:'1px solid red', padding:'20px 10px', margin:'5px', backgroundColor:'white', borderRadius:'5px'}} 
+            counterStyle={{fontSize:'15px', color:'grey', border:'1px solid red', padding:'10px', margin:'5px', backgroundColor:'white', borderRadius:'5px'}} 
             labelStyle={{color:'grey',fontSize:'12px', textTransform:'uppercase'}}
             currentDateStyle={{backgroundColor:'red',color:'white'}}
             selectedDateStyle={{backgroundColor:'green',color:'white',border:'1px solid green'}}
--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,9 @@
-import React,  { Component, Fragment } from "react";
+import React, { Component, Fragment } from "react";
 import moment from 'moment';
 
 class TimerSegment extends Component {
 
-  defaultDate = new Date(new Date().setDate(new Date().getDate()+1))
+  defaultDate = new Date(new Date().setDate(new Date().getDate() + 1))
 
   leadingZero(num) {
     return num < 10 ? "0" + num : num;
@@ -27,75 +27,83 @@ class TimerSegment extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      days: 0,
-      hours: 0,
-      minutes: 0,
-      seconds: 0,
-      deadline: this.props.date ? this.props.date : this.defaultDate,
-      activeWeek:[],
-      months:moment.months(),
+      activeWeek: [],
+      months: moment.months(),
       years: [],
-      selectedDate: moment()
-      // deadline: this.props.date ? this.props.date : "April, 6, 2019"
+      selectedDate: moment(),
+      display: window.screen.width > 450 ? 'desktop' : 'mobile',
+      activeMonth: moment().format("MMMM"),
+      activeYear:moment().year()
     };
   }
 
   componentDidMount() {
     let yearData = [];
-    for(let i = 0;i<100;i++){
-      yearData.push(moment().add(i,'years').year())
+    for (let i = 0; i < 100; i++) {
+      yearData.push(moment().add(i, 'years').year())
     }
     this.setState({
       years: yearData
     })
-   
+
     this.getCurrentWeek()
   }
 
-  getCurrentWeek=(i)=>{
+  getCurrentWeek = (i) => {
     var currentDate = i ? moment(i) : moment();
     var weekStart = currentDate.clone().startOf('isoweek');
     var days = [];
-    
+
     for (var i = 0; i <= 6; i++) {
-        days.push({
-          label: moment(weekStart).add(i, 'days').format('MMMM Do,ddd'),
-          value:  moment(weekStart).add(i, 'days').toISOString()
-        })
+      days.push({
+        label: { date: moment(weekStart).add(i, 'days').format('ddd'), day: moment(weekStart).add(i, 'days').format('DD') },
+        value: moment(weekStart).add(i, 'days').toISOString()
+      })
     }
 
     this.setState({
-      activeWeek:days
+      activeWeek: days
     })
   }
 
-  getNextWeek=()=>{
-    let dd = this.state.activeWeek[this.state.activeWeek.length-1]
-    this.getCurrentWeek(moment(dd.value).add('days',1).toISOString())
+  getNextWeek = () => {
+    let dd = this.state.activeWeek[this.state.activeWeek.length - 1]
+    let dd2 = moment(dd.value).add('days', 1).toISOString()
+    this.setState({
+      activeMonth: moment(dd2).format("MMMM"),
+      activeYear: moment(dd2).year()
+    })
+    this.getCurrentWeek(dd2)
   }
 
-  getPrevWeek=()=>{
-    let dd = this.state.activeWeek[0]
-    this.getCurrentWeek(moment(dd.value).subtract('days',1).toISOString())
+  getPrevWeek = () => {
+    let dd = this.state.activeWeek[0];
+    let dd2 = moment(dd.value).subtract('days', 1).toISOString()
+    this.setState({
+      activeMonth: moment(dd2).format("MMMM"),
+      activeYear: moment(dd2).year()
+    })
+    this.getCurrentWeek(dd2)
   }
 
-  getCurrentWeekOnChange=(type,value)=>{
-    if(type == 'year'){
-      let dd = moment(this.state.selectedDate).set('year',parseInt(value))
-
+  getCurrentWeekOnChange = (type, value) => {
+    if (type == 'year') {
+      let dd = moment(this.state.selectedDate).set('year', parseInt(value))
       this.setState({
-        selectedDate:dd
+        selectedDate: dd,
+        activeMonth: moment(dd).format("MMMM"),
+        activeYear: moment(dd).year()
       })
       this.getCurrentWeek(dd.toISOString())
-
     }
-    if(type == 'month'){
-      let mm = moment().month(value).format("M");
-    
-      let dd = moment(this.state.selectedDate).set('month',mm)
 
+    if (type == 'month') {
+      let mm = moment().month(value).format("M");
+      let dd = moment(this.state.selectedDate).set('month', mm-1)
       this.setState({
-        selectedDate:dd
+        selectedDate: dd,
+        activeMonth: moment(dd).format("MMMM"),
+        activeYear: moment(dd).year()
       })
       this.getCurrentWeek(dd.toISOString())
     }
@@ -103,8 +111,7 @@ class TimerSegment extends Component {
 
 
   render() {
-    const { days, hours, minutes, seconds } = this.state;
-    let counterStyle = this.props.counterStyle ? this.props.counterStyle  : {};
+    let counterStyle = this.props.counterStyle ? this.props.counterStyle : {};
     let timerStyle = this.props.timerStyle ? this.props.timerStyle : {};
     let labelStyle = this.props.labelStyle ? this.props.labelStyle : {};
     let currentDateStyle = this.props.currentDateStyle ? this.props.currentDateStyle : {};
@@ -112,69 +119,72 @@ class TimerSegment extends Component {
 
     return (
       <div style={timerStyle} >
-          <div style={{padding:'0px',textAlign:'center', height:'100%',background:'none'}}  >
-            <div>
-            <select onChange={(e)=>{
-         
-              this.getCurrentWeekOnChange('month',e.target.value)
+        <div style={{ padding: '0px', textAlign: 'center', height: '100%', background: 'none' }}  >
+          <div style={{marginBottom:'20px'}}>
+            <select value={this.state.activeMonth}
+            onChange={(e) => {
+              this.getCurrentWeekOnChange('month', e.target.value)
             }}>
-              {this.state.months.map((a)=>{
-                return(
+              {this.state.months.map((a) => {
+                return (
                   <option value={a}>{a}</option>
                 )
               })}
             </select>
-            <select onChange={(e)=>{
-     
-              this.getCurrentWeekOnChange('year',e.target.value)
-
+            <select value={this.state.activeYear}
+            onChange={(e) => {
+              this.getCurrentWeekOnChange('year', e.target.value)
             }}>
-              {this.state.years.map((a)=>{
-                return(
+              {this.state.years.map((a) => {
+                return (
                   <option value={a}>{a}</option>
                 )
               })}
             </select>
-            </div>
-            {/* <p style={{color: labelStyle.color ? labelStyle.color : 'grey', marginBottom:'0px'}}>STARTS IN</p> */}
+          </div>
+          {/* <p style={{color: labelStyle.color ? labelStyle.color : 'grey', marginBottom:'0px'}}>STARTS IN</p> */}
 
-            <div style={{display:'flex', justifyContent:'center'}}>
-              <button onClick={()=>{
-                this.getPrevWeek()
-              }}>Prev</button>
-              {this.state.activeWeek.map((a)=>{
-                let containerStyle = {...counterStyle};
-                let currentDateFlag = moment().format('DD-MM-YYYY') == moment(a.value).format('DD-MM-YYYY')
+          <div style={{ display: 'flex', justifyContent: 'center', width: '100%'}}>
+            <button onClick={() => {
+              this.getPrevWeek()
+            }}>Prev</button>
+            {this.state.activeWeek.map((a) => {
+              let obj = {flex:1, height: '100px', cursor: 'pointer', display:'flex',flexDirection:'column',justifyContent:'center', alignContent:'center' }
+              let containerStyle = { ...obj,...counterStyle };
+              let currentDateFlag = moment().format('DD-MM-YYYY') == moment(a.value).format('DD-MM-YYYY')
 
-                if(currentDateFlag == true){
-                  containerStyle = {...containerStyle,...currentDateStyle}
-                }
+              if (currentDateFlag == true) {
+                containerStyle = { ...containerStyle, ...currentDateStyle }
+              }
 
-                let selectedDateFlag = this.state.selectedDate ? moment(a.value).format('DD-MM-YYYY') == moment(this.state.selectedDate).format('DD-MM-YYYY') : false;
-                
-                if(selectedDateFlag == true){
-                  containerStyle = {...containerStyle,...selectedDateStyle}
-                }
-             
-                return(
-                  <div style={{flex:1, cursor:'pointer'}}
-                  onClick={()=>{
+              let selectedDateFlag = this.state.selectedDate ? moment(a.value).format('DD-MM-YYYY') == moment(this.state.selectedDate).format('DD-MM-YYYY') : false;
+
+              if (selectedDateFlag == true) {
+                containerStyle = { ...containerStyle, ...selectedDateStyle }
+              }
+
+              return (
+                <div style={containerStyle}
+                  onClick={() => {
                     this.setState({
-                      selectedDate : moment(a.value)
+                      selectedDate: moment(a.value)
                     })
                   }}>
-                  <h1 className="timer-counter" style={containerStyle}>{a.label}</h1>
+                 
+                    <h2 style={{ margin: '0px' }}>{a.label.day}</h2>
+                    <span style={{textTransform:'uppercase'}}>{a.label.date}</span>
+             
                   {/* <p style={labelStyle}>Days</p> */}
-              </div>
-                )
-              })}
-            <button onClick={()=>{
+                </div>
+              )
+            })}
+            <button onClick={() => {
               this.getNextWeek()
             }}>Next</button>
-            </div>
-            
+          </div>
+
         </div>
-        </div>
+      </div>
     );
   }
 }
